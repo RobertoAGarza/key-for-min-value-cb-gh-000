a78a9098ea71b088809f14819eac61e7bf564436
Roberto Garza <robertg4567@hotmail.com>
2018-07-23 14:39:18 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,9 @@ def key_for_min_value(name_hash)
   minKey = "none"
   minValue = 100
 
-  if name_hash.is_empty?
+  if name_hash.empty?
     return nil
-  
+
   name_hash.each do |key, value|
     if minValue > value
       minValue = value
